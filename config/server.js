765dc1706f55f183f191b1c4db28afcaa966e649
Alan Killian <akillian@outlook.com>
2020-08-25 19:55:46 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '08520df5148a1743fb863a5a486af096'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b73eb877-f86d-46b0-aad5-c21d9cac6bc4'
+};
